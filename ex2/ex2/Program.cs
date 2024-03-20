f42acf388bd3365b0d88171933e1d87e12cac624
Jakub Frelik <git@jfrelik.dev>
2024-03-20 17:44:36 +0100

    init project

---
+++ ex2/ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ex2;
+using ex2.utils;
+
+Console.WriteLine("Hello, World!");
+
+ShipContainer cont = new ShipContainer(100, 20, 20, 20,ContainerType.Cooler);
+Cargo cargo = new Cargo(100, "Bananas");
+
+Console.WriteLine(cont.ToString());
